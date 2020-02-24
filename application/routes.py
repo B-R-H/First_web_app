6a95fe65155d8c736b00882942579de97570ac96
B-R-H <barnaby66@gmail.com>
2020-02-24 14:20:57 +0000

    initial commit

---
+++ application/routes.py
@@ -0,0 +1,26 @@
+# import render_template function from the flask module
+from flask import render_template
+# import the app object from the ./application/__init__.py
+from application import app
+from application.models import Posts
+
+
+# define routes for / & /home, this function will be called when these are accessed
+@app.route('/')
+@app.route('/home')
+def home():
+    postData = Posts.query.first()
+    return render_template('home.html', title='Home', post=postData)
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
+
+@app.route('/login')
+def login():
+    return render_template('login.html', title='Log in')
+
+@app.route('/register')
+def register():
+    return render_template('register.html', title='Register')
+
