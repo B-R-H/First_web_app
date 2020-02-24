392dd69256c3235fb16ff3de0869645b1a6ae8ce
B-R-H <barnaby66@gmail.com>
2020-02-24 16:25:25 +0000

    web app with form for data entry

--- application/__init__.py
+++ application/__init__.py
@@ -4,6 +4,8 @@ import os
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = str(os.getenv('MY_SECRET_KEY'))
+
 app.config['SQLALCHEMY_DATABASE_URI'] = str(os.getenv('DATABASE_URI'))
 db = SQLAlchemy(app)
 
